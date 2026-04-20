acb89aff1f19a59485e1b23fcba5fe4421ba5c4e
Realsunnysagar <realsunnysagar@gmail.com>
2026-04-20 23:42:32 +0530

    Added Strings

---
+++ LeetCode/02-Strings/3.Longest-Substring-Without-Repeating-Characters.cpp
@@ -0,0 +1,9 @@
+/*
+Longest substring without repeating characters
+    s = c a d b z a b c d
+    possible substring --> cabdz, zabcd ...  you will find that max of 5 will come as a ans in this case
+
+    //optimal
+    start to thing about sliding window and 2 pointers'
+    
+*/ 
