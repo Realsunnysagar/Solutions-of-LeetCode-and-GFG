60a8bab97a03909ebd6caa6e13ba85c599ff4cc6
Sunny Sagar <143802341+Realsunnysagar@users.noreply.github.com>
2026-07-22 12:50:52 +0530

    Time: 0 ms (100%), Space: 8 MB (74.85%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int minOpen = 0, maxOpen = 0;
+        int n = s.size();
+        for(int i = 0; i<n ; i++){
+            if(s[i] == '('){
+                minOpen++;
+                maxOpen++;
+            }else if(s[i] == ')'){
+                minOpen--;
+                maxOpen--;
+            }else{
+                minOpen = minOpen - 1;
+                maxOpen = maxOpen + 1;
+            }
+            if(maxOpen < 0) return false;
+            if(minOpen < 0) minOpen = max(minOpen, 0);
+        }
+        return (minOpen == 0);
+    }
+};
